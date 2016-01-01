97a795e194361f8f44593ef638b9dffb5ac8e01a
Mackie Loeffel <mackie.loeffel@web.de>
2016-01-01 13:52:39 +0100

    table classes from database

--- table.php
+++ table.php
@@ -7,105 +7,42 @@
 
 <table class=".abstand" style="margin-left: 7px; color= white; margin-right= 14px;" cellspacing="0" cellpadding="20"> <thead style="color: white;"><tr> <th  style="text-align: center">Rang</th> <th style="text-align: center">Klassenname</th> <th style="text-align: center">Challenges</th> <th style="text-align: center">Kreativität</th> <th style="text-align: center">Punkte</th> </tr></thead>
     <tbody>
+        <?php
+        $classStmt = $db->prepare("SELECT id, name FROM class");
+        $classStmt->execute();
+        foreach($classStmt->fetchAll(PDO::FETCH_OBJ) as $row) {
+        ?>
         <tr>
             <td  style="color: white; text-align: center"><b>1</b></td>
-            <td style="text-align: center">Die Sojapatronen</td>
+            <td style="text-align: center"><?= $row->name ?></td>
             <td style="text-align: center">
-
-
-
-                <div style="background-color:#FCC156;
-                            width: 20px;
-                            height: 20px;
-                            border-radius:20px;
-                            margin-top: 0px;
-                            margin-right: 0px;
-                            margin-bottom: 0px;
-                            margin-left: 0px;
-                            text-align:center;
-                            color: black;
-                            font-size: 12px;
-                            font-width: bold;
-                            float: left;
-                            ">
+                <div class="table-box" style="background-color:#FCC156;">
                     <b>2</b>
-                    <span style="background-color:#3A7EFC;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
-                                 margin-left: 25px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
-                                 ">
+                    <span class="table-number"
+                          style="background-color:#3A7EFC;">
                         <b>1</b>
                     </span>
-                    <span style="background-color:#7761C5;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
+                    <span class="table-number"
+                          style="background-color:#7761C5;
                                  margin-left: 50px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
                                  ">
                         <b>1</b>
                     </span>
-                    <span style="background-color:#1B5A0A;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
+                    <span class="table-number"
+                          style="background-color:#1B5A0A;
                                  margin-left: 75px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
                                  ">
                         <b>1</b>
                     </span>
-                    <span style="background-color:#CC4321;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
+                    <span class="table-number"
+                          style="background-color:#CC4321;
                                  margin-left: 100px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
                                  ">
                         <b>0</b>
                     </span>
-                    <span style="background-color:white;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
+                    <span class="table-number"
+                          style="background-color:white;
                                  margin-left: 125px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
                                  ">
                         <b>1</b>
                     </span>
@@ -113,214 +50,7 @@
                     <!--Anzahl bestandener Challenges pro Kategorie--></td>
             <td style="text-align: center"><!--1+(0,2*anzahlSelfmade-Challenges)-->1,4</td>
             <td style="text-align: center"><b><!--Punkte der bestandenen Challenges * Kreativität-->16,8</b></td> </tr>
-        <tr>
-            <td  style="color: white; text-align: center"><b>2</b></td>
-            <td style="text-align: center">Elektrokürbis</td>
-            <td>
-
-                <div style="background-color:#FCC156;
-                            width: 20px;
-                            height: 20px;
-                            border-radius:20px;
-                            margin-top: 0px;
-                            margin-right: 0px;
-                            margin-bottom: 0px;
-                            margin-left: 0px;
-                            text-align:center;
-                            color: black;
-                            font-size: 12px;
-                            font-width: bold;
-                            float: left;
-                            ">
-                    <b>2</b>
-                    <span style="background-color:#3A7EFC;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
-                                 margin-left: 25px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
-                                 ">
-                        <b>0</b>
-                    </span>
-                    <span style="background-color:#7761C5;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
-                                 margin-left: 50px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
-                                 ">
-                        <b>0</b>
-                    </span>
-                    <span style="background-color:#1B5A0A;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
-                                 margin-left: 75px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
-                                 ">
-                        <b>1</b>
-                    </span>
-                    <span style="background-color:#CC4321;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
-                                 margin-left: 100px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
-                                 ">
-                        <b>0</b>
-                    </span>
-                    <span style="background-color:white;
-                                 width: 20px;
-                                 height: 20px;
-                                 border-radius:20px;
-                                 margin-top: -24px;
-                                 margin-right: 0px;
-                                 margin-bottom: 0px;
-                                 margin-left: 125px;
-                                 text-align:center;
-                                 color: black;
-                                 font-size: 12px;
-                                 font-width: bold;
-                                 float: left;
-                                 ">
-                        <b>1</b>
-                    </span>
-            </td>
-            <td style="text-align: center">1,2</td>
-            <td style="text-align: center"><b>12</b></td>
-        </tr>
-
-        <tr>
-            <td  style="color: white; text-align: center"><b>3</b></td>
-            <td style="text-align: center">Mc Do Not</td>
-            <td><div style="background-color:#FCC156;
-                            width: 20px;
-                            height: 20px;
-                            border-radius:20px;
-                            margin-top: 0px;
-                            margin-right: 0px;
-                            margin-bottom: 0px;
-                            margin-left: 0px;
-                            text-align:center;
-                            color: black;
-                            font-size: 12px;
-                            font-width: bold;
-                            float: left;
-                            ">
-                <b>2</b>
-                <span style="background-color:#3A7EFC;
-                             width: 20px;
-                             height: 20px;
-                             border-radius:20px;
-                             margin-top: -24px;
-                             margin-right: 0px;
-                             margin-bottom: 0px;
-                             margin-left: 25px;
-                             text-align:center;
-                             color: black;
-                             font-size: 12px;
-                             font-width: bold;
-                             float: left;
-                             ">
-                    <b>0</b>
-                </span>
-                <span style="background-color:#7761C5;
-                             width: 20px;
-                             height: 20px;
-                             border-radius:20px;
-                             margin-top: -24px;
-                             margin-right: 0px;
-                             margin-bottom: 0px;
-                             margin-left: 50px;
-                             text-align:center;
-                             color: black;
-                             font-size: 12px;
-                             font-width: bold;
-                             float: left;
-                             ">
-                    <b>0</b>
-                </span>
-                <span style="background-color:#1B5A0A;
-                             width: 20px;
-                             height: 20px;
-                             border-radius:20px;
-                             margin-top: -24px;
-                             margin-right: 0px;
-                             margin-bottom: 0px;
-                             margin-left: 75px;
-                             text-align:center;
-                             color: black;
-                             font-size: 12px;
-                             font-width: bold;
-                             float: left;
-                             ">
-                    <b>1</b>
-                </span>
-                <span style="background-color:#CC4321;
-                             width: 20px;
-                             height: 20px;
-                             border-radius:20px;
-                             margin-top: -24px;
-                             margin-right: 0px;
-                             margin-bottom: 0px;
-                             margin-left: 100px;
-                             text-align:center;
-                             color: black;
-                             font-size: 12px;
-                             font-width: bold;
-                             float: left;
-                             ">
-                    <b>0</b>
-                </span>
-                <span style="background-color:white;
-                             width: 20px;
-                             height: 20px;
-                             border-radius:20px;
-                             margin-top: -24px;
-                             margin-right: 0px;
-                             margin-bottom: 0px;
-                             margin-left: 125px;
-                             text-align:center;
-                             color: black;
-                             font-size: 12px;
-                             font-width: bold;
-                             float: left;
-                             ">
-                    <b>1</b>
-                </span></td>
-            <td style="text-align: center">1</td>
-            <td style="text-align: center"><b>10</b></td>
-        </tr>
-
-
+        <?php } ?>
     </tbody>
 </table>
 
