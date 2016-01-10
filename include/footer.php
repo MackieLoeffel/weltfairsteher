971becf68d89d47811c60cd80a6b00b1921375cf
Mackie Loeffel <mackie.loeffel@web.de>
2016-01-10 14:44:31 +0100

    cleanup and reformat footer

--- include/footer.php
+++ include/footer.php
@@ -1,35 +1,35 @@
 
 <!--<footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="textwidget"><ul id="menu-footer1" class="menu">
-    </div>
-    </aside><aside id="text-87" class="widget widget_text"><div class="widget-title">Kontakt</div>
-        <div class="site-info">
-            Copyright &copy; 2016 <br>CHANGE e.V. - Chancen.Nachhaltig.Gestalten. <br> Mitglieder des Max-Weber-Programms <br>
-            Alle Rechte vorbehalten.<br>
+     <div class="textwidget"><ul id="menu-footer1" class="menu">
+     </div>
+     </aside><aside id="text-87" class="widget widget_text"><div class="widget-title">Kontakt</div>
+     <div class="site-info">
+     Copyright &copy; 2016 <br>CHANGE e.V. - Chancen.Nachhaltig.Gestalten. <br> Mitglieder des Max-Weber-Programms <br>
+     Alle Rechte vorbehalten.<br>
 
-            <a href="http://www.chancengestalten.de.com" title="CHANGE">Über CHANGE e.V.</a> </div>
-</footer>-->
+     <a href="http://www.chancengestalten.de.com" title="CHANGE">Über CHANGE e.V.</a> </div>
+     </footer>-->
 
-  <div>
+<div>
     <footer style="
-    z-index: 5;
-    width: 100%;
-    height: 60px;
-    background-color: #027018;
-  color: #CACCCA;
-  font-size: 11px;
-  text-align: center;
-  margin-top: 60em;
-  border-top: 2px solid black;
-position: static;
-bottom: 0px;
+                   z-index: 5;
+                   width: 100%;
+                   height: 60px;
+                   background-color: #027018;
+                   color: #CACCCA;
+                   font-size: 11px;
+                   text-align: center;
+                   margin-top: 60em;
+                   border-top: 2px solid black;
+                   position: static;
+                   bottom: 0px;
 
-  ">
+                   ">
 
-  <br>
-   &copy; 2016 WeltFAIRsteher <br> Kontakt: weltfairsteher@chancengestalten.de
-  </footer> </div>
+        <br>
+        &copy; 2016 WeltFAIRsteher <br> Kontakt: weltfairsteher@chancengestalten.de
+    </footer> </div>
 
-    </div>
+     </div>
     </body>
     </html>
