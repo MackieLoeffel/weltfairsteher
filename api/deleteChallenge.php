e1c243ed9133124f620ec378926e2f201a123232
Mackie Loeffel <mackie.loeffel@web.de>
2016-02-01 23:09:02 +0100

    can delete selfmade challenges

--- api/deleteChallenge.php
+++ api/deleteChallenge.php
@@ -3,14 +3,16 @@ include __DIR__."/include.php";
 
 check_access(ADMIN);
 
-list($challenge) = apiCheckParams("challenge");
+list($suggested, $challenge) = apiCheckParams("suggested", "challenge");
 
-apiCheck(dbExists("SELECT id FROM challenge WHERE id = :challenge",
+$table = $suggested ? "suggested" : "challenge";
+
+apiCheck(dbExists("SELECT id FROM $table WHERE id = :challenge",
                   ['challenge' => $challenge]),
          "Challenge existiert nicht!");
 
-apiAction(function() use($challenge) {
-    dbExecute("DELETE FROM challenge WHERE id = :challenge",
+apiAction(function() use($table, $challenge) {
+    dbExecute("DELETE FROM $table WHERE id = :challenge",
               ['challenge' => $challenge]);
 });
 ?>
