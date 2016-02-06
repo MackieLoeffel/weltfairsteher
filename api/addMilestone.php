131b4fafc12d9c72b015e3bf748dad46d6e53a14
Mackie Loeffel <mackie.loeffel@web.de>
2016-02-06 15:21:07 +0100

    fixed addMilestone

--- api/addMilestone.php
+++ api/addMilestone.php
@@ -5,8 +5,10 @@ check_access(ADMIN);
 
 list($points, $description) = apiCheckParams("points", "description");
 
-apiCheck(strlen($description) !== 0, "Beschreibung darf nicht leer sein");
 apiCheck(ctype_digit($points), "Punkte müssen eine Zahl sein");
+apiCheck(!dbExists("SELECT id FROM milestone WHERE points = :p", ["p"=>$points]),
+         "Punkte schon vorhanden");
+apiCheck(strlen($description) !== 0, "Beschreibung darf nicht leer sein");
 
 apiAction(function() use($points, $description) {
     dbExecute("INSERT INTO milestone (points, description) VALUES (:points, :description)",
