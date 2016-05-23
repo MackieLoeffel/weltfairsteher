f37fd587a290e5ecab8db9ff3f4567e6d7f09c42
Mackie Loeffel <mackie.loeffel@web.de>
2016-05-23 23:35:06 +0200

    send mail on new feedback

--- api/addFeedback.php
+++ api/addFeedback.php
@@ -6,10 +6,10 @@ check_access(TEACHER);
 list($challenge, $fun, $integration, $duration, $problems, $comment) = apiCheckParams("challenge", "fun", "integration", "duration", "problems", "comment");
 
 apiCheck(ctype_digit($fun) && ctype_digit($integration) && ctype_digit($duration) && ctype_digit($problems), "Werte müssen Zahlen sein!");
-apiCheck(dbExists("SELECT id FROM challenge WHERE id = :id", ['id' => $challenge]),
-         "Unbekannte Challenge");
+$challengeRow = fetch("SELECT name FROM challenge WHERE id = :id", ["id" => $challenge]);
+apiCheck($challengeRow !== false, "Unbekannte Challenge");
 
-apiAction(function() use($challenge, $fun, $integration, $duration, $problems, $comment) {
+apiAction(function() use($challenge, $fun, $integration, $duration, $problems, $comment, $challengeRow) {
     dbExecute("INSERT INTO feedback (challenge, fun, integration, duration, problems, comment) VALUES (:challenge, :fun, :integration, :duration, :problems, :comment)",
               ["challenge" => $challenge,
                "fun" => $fun,
@@ -17,5 +17,7 @@ apiAction(function() use($challenge, $fun, $integration, $duration, $problems, $
                "duration" => $duration,
                "problems" => $problems,
                "comment" => $comment]);
+
+    own_mail("kontakt@weltfairsteher.jetzt", "Neues Feedback", "Es ist ein neues Feedback für die Challenge " . e($challengeRow->name) . " eingegangen.\r\nGehe auf www.weltfairsteher.jetzt/feedback.php zu anzeigen!");
 });
 ?>
