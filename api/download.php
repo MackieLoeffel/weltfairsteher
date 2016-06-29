350db2ba8f8da8c244afb7e037027aa7d81f9d05
Mackie Loeffel <mackie.loeffel@web.de>
2016-06-29 22:18:15 +0200

    changed filename for downloaded pdfs

--- api/download.php
+++ api/download.php
@@ -2,9 +2,10 @@
 include __DIR__."/include.php";
 
 list($challenge, $type) = apiCheckParams("challenge", "type");
-apiCheck(dbExists("SELECT id FROM challenge WHERE id = :challenge",
-                  ['challenge' => $challenge]),
-         "Challenge existiert nicht!");
+
+$challengeRow = fetch("SELECT name FROM challenge WHERE id = :challenge",
+                  ['challenge' => $challenge]);
+apiCheck($challengeRow !== false, "Challenge existiert nicht!");
 if($type !== TEACHER_PDF && $type !== PUPIL_PDF) {
     apiAddError("Ungültiger Typ");
 } else {
@@ -16,12 +17,28 @@ if($type !== TEACHER_PDF && $type !== PUPIL_PDF) {
     }
 }
 
-apiAction(function() use($file) {
+apiAction(function() use($file, $challengeRow, $type) {
+    $filename = $challengeRow->name;
+    // normalize filename
+    // idea from http://stackoverflow.com/questions/2021624/string-sanitizer-for-filename
+    $filename = mb_ereg_replace("(ä)", 'a', $filename);
+    $filename = mb_ereg_replace("(ü)", 'u', $filename);
+    $filename = mb_ereg_replace("(ö)", 'o', $filename);
+    $filename = mb_ereg_replace("(Ä)", 'A', $filename);
+    $filename = mb_ereg_replace("(Ü)", 'U', $filename);
+    $filename = mb_ereg_replace("(Ö)", 'O', $filename);
+    $filename = mb_ereg_replace("([^A-Za-z_0-9])", '', $filename);
+
+    if($type === TEACHER_PDF)
+    {
+        $filename .= "_Lehrer";
+    }
+
     // see http://stackoverflow.com/a/27805443
     header('Content-Type: application/pdf');
 
     //Use Content-Disposition: attachment to specify the filename
-    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Disposition: attachment; filename=' . $filename . ".pdf");
 
     //No cache
     header('Expires: 0');
