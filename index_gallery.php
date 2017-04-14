6ecf245e3f784ff1d09ecb30f30c51494fcce64f
Andreas Eichenseher <andreas-eichenseher@web.de>
2017-04-14 21:28:14 +0200

    .svg-dateiendung angepasst

--- index_gallery.php
+++ index_gallery.php
@@ -1,9 +1,9 @@
-  
+
         <link rel="stylesheet" type="text/css" href="index_gallery/css/rslides_layout.css">
         <link rel="stylesheet" type="text/css" href="index_gallery/css/index_gallery_layout.css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
         <script src="index_gallery/js/responsiveslides.min.js"></script>
-        
+
         <script>
           $(function() {
             $(".rslides").responsiveSlides({
@@ -21,28 +21,28 @@
         <div class="callbacks_container">
             <ul class="rslides">
                 <li>
-                  <img src="index_gallery/images/Slide1.SVG" alt="">
+                  <img src="index_gallery/images/Slide1.svg" alt="">
                 </li>
                 <li>
-                  <img src="index_gallery/images/Slide2.SVG" alt="">
+                  <img src="index_gallery/images/Slide2.svg" alt="">
                 </li>
                 <li>
-                  <img src="index_gallery/images/Slide3.SVG" alt="">
+                  <img src="index_gallery/images/Slide3.svg" alt="">
                 </li>
                 <li>
-                  <img src="index_gallery/images/Slide4.SVG" alt="">
+                  <img src="index_gallery/images/Slide4.svg" alt="">
                 </li>
                 <li>
-                  <img src="index_gallery/images/Slide5.SVG" alt="">
+                  <img src="index_gallery/images/Slide5.svg" alt="">
                 </li>
                 <li>
-                  <img src="index_gallery/images/Slide6.SVG" alt="">
+                  <img src="index_gallery/images/Slide6.svg" alt="">
                 </li>
                 <li>
-                  <img src="index_gallery/images/Slide7.SVG" alt="">
+                  <img src="index_gallery/images/Slide7.svg" alt="">
                 </li>
                 <li>
-                  <img src="index_gallery/images/Slide8.SVG" alt="">
+                  <img src="index_gallery/images/Slide8.svg" alt="">
                 </li>
             </ul>
         </div>
