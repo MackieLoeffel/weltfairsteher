20a55c2f366e09b0377c82e4ff7c7a9d2eb5a21b
Mackie Loeffel <mackie.loeffel@web.de>
2017-08-16 21:24:27 +0200

    Added Admin tests and flyway

--- api/deleteTeacher.php
+++ api/deleteTeacher.php
@@ -12,9 +12,8 @@ apiCheck(dbExists("SELECT id FROM user WHERE id = :teacher AND role != :admin",
 apiCheck(!dbExists("SELECT id FROM class WHERE teacher = :id", ["id" => $teacher]),
          "Benutzer kann nur entfernt werden, wenn er keine Klassen mehr hat.");
 
-apiAction(function() use($teacher, $db) {
-    $statement = $db->prepare("DELETE FROM user WHERE id = :teacher");
-    $result = $statement->execute(['teacher' => $teacher]);
-    dbExecute("DELETE FROM forgot WHERE user = :id", ["id" => $id]);
+apiAction(function() use($teacher) {
+    dbExecute("DELETE FROM user WHERE id = :id", ["id" => $teacher]);
+    dbExecute("DELETE FROM forgot WHERE user = :id", ["id" => $teacher]);
 });
 ?>
